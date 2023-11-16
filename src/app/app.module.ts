4d0d7b5e94b492967ee3692904383aea7584d0cc
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-11-16 17:12:34 +0100

    service and search component implementations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms"
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 import { RouterModule, Routes } from '@angular/router';
-
+import { SearchComponent } from './search/search.component';
+import { HttpClientModule } from '@angular/common/http';
 const routes: Routes = [
+  {path: "", redirectTo: "search", pathMatch: "full"},
   {
-    path: 'home',
-    component: HomeComponent
+    path: 'search',
+    component: SearchComponent
   },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent
-  }
+  
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,80 @@
+import { 
+  Component, 
+  OnInit 
+} from '@angular/core';
+
+import { 
+  Router, 
+  ActivatedRoute
+} from '@angular/router'
+import { SpotifyService } from '../spotify.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+
+
+
+export class SearchComponent implements OnInit {
+  query: string = ''
+  results: {
+    name: string,
+    artists: {
+      id: number,
+      name: string
+    }[],
+    
+    album: {
+      id: number,
+      name: string,
+      images: {
+        url: string
+      }[]
+    },
+    id: string
+  }[] | null = null
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private spotify: SpotifyService
+  ) {
+    this.route
+      .queryParams
+      .subscribe(params => { 
+        this.query = params['query'] || ''
+      })
+    }
+
+  search(): void {
+    console.log("this.query", this.query);
+    if (!this.query) {
+      return
+    }
+    
+    this.spotify
+      .searchTrack(this.query)
+      .subscribe((res: any) => {
+        this.renderResults(res)
+      })
+  }
+  
+  renderResults(res: any): void {
+    this.results = null
+    if (res && res.tracks && res.tracks.items) {
+      this.results = res.tracks.items
+    }
+  }
+
+  submit(query: string) {
+    this.router
+      .navigate(['search'], { queryParams: { query: query } })
+      .then(_ => this.search())
+  }
+
+  ngOnInit(): void {
+    this.search()
+  }
+}
