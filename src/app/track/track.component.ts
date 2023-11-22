ab503f8006e1f70adfd8deeffb3d383a9cf0098a
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-11-22 17:37:52 +0100

    authentication and routing implementations

--- src/app/track/track.component.ts
+++ src/app/track/track.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
   styleUrls: ['./track.component.css']
 })
-export class TrackComponent {
+export class TrackComponent implements OnInit {
   id: string | null = null
   track: {
     name: string,
@@ -26,11 +27,27 @@ export class TrackComponent {
 
   } | null = null
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private spotify: SpotifyService
+  ) {
       route.params.subscribe(params => {
         this.id = params['id']
      })
   }
   back() {
+  }
+
+  ngOnInit(): void {
+
+   if (this.id) {
+     this.spotify
+       .getTrack(this.id)
+       // create a subscribe method that renderss the tracts
+       .subscribe((data: any) => {
+          this.track = data
+        }
+      )
+   }
   }
 }
